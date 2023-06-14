87732899cc8e6ebcc1e3a39adb4212e117b798d7
bryan <bmklei8p@gmail.com>
2023-06-14 14:51:23 -0400

    trying just * wildcard for origins

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ from config import settings
 app = FastAPI()
 
 origins = [
-    "http://localhost"
-    "http://localhost:3000", 
-    "http://localhost:3000/",
-    "http://localhost:8000",
-    "http://localhost:8000/",
+    "*",
 ]
 
 app.add_middleware(
