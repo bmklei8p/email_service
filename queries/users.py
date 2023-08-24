1b787367e3aa56ddf39561e7ee16653c555fad72
bryan <bmklei8p@gmail.com>
2023-08-24 16:38:34 -0400

    updated db name to match mongodb

--- queries/users.py
+++ queries/users.py
@@ -2,9 +2,12 @@ from .client import Queries
 from models import User
 
 class UserQueries(Queries):
-    DB_NAME = "email_api"
+    DB_NAME = "email_service"
     COLLECTION = "users"
     
     def get_one(self, email):
         user = self.collection.find_one({"email": email})
-        return User(**user) 
+        if user:
+           return User(**user) 
+        else:
+            return None
